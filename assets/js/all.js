7e3319394cd4143ec32f607d37c1200ede76d0ff
sasimi1327 <wonderland1327@gmail.com>
2022-09-14 21:12:35 +0800

    Update 2022-09-14T13:12:25.721Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,7 +1,13 @@
 "use strict";
 
-console.log('Hello!');
-$(document).ready(function () {
-  console.log('HesSchool Hello!');
-});
+var pad = window.matchMedia("(max-width: 992px)");
+pad.addListener(checkMenuClose);
+
+function checkMenuClose(pMatchMedia) {
+  if (pMatchMedia.matches) {
+    console.log("小於992");
+  } else {
+    console.log("大於992");
+  }
+}
 //# sourceMappingURL=all.js.map
