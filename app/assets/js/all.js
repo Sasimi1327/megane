44941bc1bcae9bf95e6d69661252cde33a945cc7
sasimi1327 <wonderland1327@gmail.com>
2022-09-15 17:48:38 +0800

    新增 系列鏡框頁

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -8,3 +8,24 @@ function checkMenuClose(pMatchMedia) {
     console.log("大於992");
   }
 }
+
+function openPanel(evt, panelName) {
+  // Declare all variables
+  var i, tabcontent, tablinks;
+
+  // Get all elements with class="tabcontent" and hide them
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  // Get all elements with class="tablinks" and remove the class "active"
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+  // Show the current tab, and add an "active" class to the button that opened the tab
+  document.getElementById(panelName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
